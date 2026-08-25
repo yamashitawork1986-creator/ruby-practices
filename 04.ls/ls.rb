a5cae489c02045b1711c60cbaa926b72f391bb11
takafumi <yamashita.work.1986@gmail.com>
2026-08-26 06:41:58 +0900

    レビュー指摘を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-files = Dir.glob('*').sort
-columns = 3
+files = Dir.glob('*')
+COLUMNS = 3
 
 def calculate_rows(files, columns)
-  (files.length.to_f / columns).ceil
+  files.length.ceildiv(columns)
 end
 
-rows = calculate_rows(files, columns)
+rows = calculate_rows(files, COLUMNS)
 
 def calculate_column_width(files)
-  max_length = files.map(&:length).max
+  max_length = files.map(&:length).max || 0
   max_length + 2
 end
 
 column_width = calculate_column_width(files)
 
 rows.times do |row|
-  columns.times do |column|
+  COLUMNS.times do |column|
     index = row + rows * column
     print files[index].ljust(column_width) if files[index]
   end
